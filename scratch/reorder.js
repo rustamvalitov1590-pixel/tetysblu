19b18a9cfe40935f848ccf1cbd6715839519136a
rustamvalitov1590-pixel <rustamvalitov1590@gmail.com>
2026-08-05 00:49:07 +0500

    Delete scratch directory

--- scratch/reorder.js
+++
@@ -1,6 +0,0 @@
-var fs = new ActiveXObject("Scripting.FileSystemObject");
-var ForReading = 1, ForWriting = 2;
-var path = "c:\\Users\\rustava\\Downloads\\Проекты\\tetis-blue-calc\\index.html";
-var ts = fs.OpenTextFile(path, ForReading, false, -1); // -1 = Unicode (UTF-16), but wait, it's UTF-8!
-var html = ts.ReadAll();
-ts.Close();
